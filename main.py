e31b6f09c42d31c13a679eeb0a479f1a52cb6f1a
Alexander Zhariuk <codo6@mail.ru>
2020-07-27 03:46:11 +0300

    Workable version

--- main.py
+++ main.py
@@ -2,3 +2,33 @@
 
 #   OUTPUT VIEW: {PLACE}: {direction}, {COUNTRY}, {MAGNITUDE}: {VALUE}
 
+
+import requests
+
+user_starttime = input('Введи старт временного промежутка: ')
+user_endtime = input('Введи конец временного промежутка: ')
+user_latitude = input('Введи широту (в десятичных градусах): ')
+user_longitude = input('Введи долготу(в десятичных градусах): ')
+user_maxradiuskm = input('Введи максимальный радиус (в км): ')
+user_minmagnitude = input('Введи минимальную магнитуду: ')
+
+url = 'https://earthquake.usgs.gov/fdsnws/event/1/query?'
+response = requests.get(url, headers={'Accept': 'application/json'}, params={
+    'format': 'geojson',
+    'starttime': user_starttime,
+    'endtime': user_endtime,
+    'latitude': user_latitude,
+    'longitude': user_longitude,
+    'maxradiuskm': user_maxradiuskm,
+    'minmagnitude': user_minmagnitude,
+})
+
+data = response.json()
+count = 0
+range_value = data['metadata']['count']
+
+for place in range(data['metadata']['count']):
+    magnitude_value = str(data['features'][count]['properties']['mag'])
+    print(str(count + 1) + '.' + 'Место: ' + data['features'][count]['properties']['place'] + '.' + ' Магнитуда: ' +
+          magnitude_value + '.')
+    count += 1
